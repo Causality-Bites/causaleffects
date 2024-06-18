d8562d3f200a7e5857a471b3daea02bf94510f9e
Lorenzo Fabbri <lorenzo.fabbri92sm@gmail.com>
2024-06-18 08:44:35 +0200

    Working on IPW estimator for intervention defined in paper fast food. Single exposure only, more than two levels of exposure.

--- R/ipw.R
+++ R/ipw.R
@@ -30,26 +30,18 @@ ipw <- function(dat,
                 cross_val,
                 method) {
   # Dispatch function
-  if (is.null(time_varying)) {
-    # Single time point
-    res <- ipw_fix()
-
-  } else {
-    # Time-varying exposures and confounders
-  }
 } # End function ipw
 ################################################################################
 
-#' Implementation of a IPW estimator (categorical exposures)
+#' Implementation of a specific IPW estimator (categorical exposures)
 #' for a single time point.
 #'
 #' @param dat Dataset with confounders, exposures, and outcome. A dataframe.
 #' @param idxs Indexes for bootstrapping. A vector of integers.
 #' @param baseline Baseline confounders. A vector of strings.
-#' @param exposures Exposure variables. A vector of strings.
+#' @param exposure Exposure variable. A string.
 #' @param outcome Outcome variable. A string.
-#' @param forms Model formula. A strings.
-#' @param shift_function Function to shift the exposures. A function.
+#' @param form Model formula. A string.
 #' @param survey_weights Optional vector of survey/balancing weights. A vector.
 #' @param SL_library Vector of learners or function. A vector of strings or function.
 #' @param cross_val TBD.
@@ -61,35 +53,16 @@ ipw <- function(dat,
 ipw_cat_fix <- function(dat,
                         idxs,
                         baseline,
-                        exposures,
+                        exposure,
                         outcome,
-                        forms,
-                        shift_function,
+                        form,
                         survey_weights,
                         SL_library,
                         cross_val) {
   # Sample for bootstrapping
   dat <- dat[idxs, ]
-
-  # Dummy variables of the exposures
-  levels_exposures <- lapply(
-    dat[[exposures]], function(x) {
-      length(unique(dat[[x]]))
-    }
-  )
-  names(levels_exposures) <- exposures
   old_colnames <- colnames(dat)
-  dat <- fastDummies::dummy_cols(
-    .data = dat,
-    select_columns = exposures,
-    remove_first_dummy = FALSE,
-    remove_most_frequent_dummy = FALSE,
-    remove_selected_columns = FALSE
-  )
-  dat <- dat |>
-    dplyr::mutate(
-      dplyr::across(setdiff(colnames(dat), old_colnames), as.factor)
-    )
+  levels_exposure <- length(unique(dat[[exposure]]))
 
   # Implementation #
   if (is.null(cross_val)) {
@@ -98,8 +71,72 @@ ipw_cat_fix <- function(dat,
     if (SL_library == "polr") {
       # Ordinal logistic model
       mod <- MASS::polr(
-        formula =
+        formula = as.formula(form),
+        data = dat,
+        weights = survey_weights,
+        na.action = "na.omit",
+        method = "logit"
       )
+
+    } else if (SL_library == "glm") {
+      # Series of logistic regression models
+      dat <- fastDummies::dummy_cols(
+        .data = dat,
+        select_columns = exposure,
+        remove_first_dummy = FALSE,
+        remove_most_frequent_dummy = FALSE,
+        remove_selected_columns = FALSE
+      )
+      dat <- dat |>
+        dplyr::mutate(
+          dplyr::across(setdiff(colnames(dat), old_colnames), as.factor)
+        )
+
+      mods <- lapply(1:(levels_exposure - 1), function(lev) {
+        dat_fit <- ifelse(
+          lev == 1,
+          dat,
+          dat[dat[[exposure]] != min(dat[[exposure]], na.rm = TRUE), ]
+        )
+        glm(
+          formula = as.formula(form),
+          data = dat_fit,
+          family = binomial(link = "logit"),
+          weights = survey_weights,
+          na.action = "na.omit"
+        )
+      }) # End fitting models
+
+      # Estimate probabilities and weights
+      for (lev in 1:levels_exposure) {
+        prob_label <- paste0("p", lev)
+        if (lev > 1 & lev < levels_exposure) {
+          dat[[prob_label]] <- stats::predict(
+            mods[[lev]], newdata = dat, type = "response"
+          ) * (1 - predict(mods[[1]], type = "response"))
+        } else if (lev == 1) {
+          dat[[prob_label]] <- predict(mods[[lev]], type = "response")
+        } else {
+          # Maximum value of the exposure
+          sum_probs <- lapply(2:(idx - 1), function(i) {
+            predict(mods[[i]], newdata = dat, type = "response")
+          }) |>
+            dplyr::bind_cols()
+          sum_probs <- apply(sum_probs, MARGIN = 1, FUN = sum)
+          dat[[prob_label]] <- (1 - sum_probs) *
+            (1 - predict(mods[[1]], type = "response"))
+        }
+      } # End estimation probabilities
+
+      dat[["weights"]] <- NA
+      dat[dat[[exposure]] == levels_exposure, "weights"] <- 0
+      dat[dat[[exposure]] == 1, "weights"] <- 1 +
+        (dat[dat[[exposure]] == 1, "p2"] / dat[dat[[exposure]] == 1, "p1"])
+      for (lev in 2:(levels_exposure - 1)) {
+        num <- dat[dat[[exposure]] == idx, paste0("p", idx + 1)]
+        den <- dat[dat[[exposure]] == idx, paste0("p", idx)]
+        dat[dat[[exposure]] == lev, "weights"] <- num / den
+      } # End estimation weights
 
     } else {
       # SL
@@ -112,7 +149,7 @@ ipw_cat_fix <- function(dat,
   } # End if for CV choice
 
   return(
-    c()
+    c(mean(dat[[outcome]]), mean(dat[["weights"]] * dat[[outcome]]))
   )
-} # End function ipw_fix
+} # End function ipw_cat_fix
 ################################################################################
