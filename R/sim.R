0ed5a87a7195e496e25f5f616addb4c99ff1a13e
Lorenzo Fabbri <lorenzo.fabbri92sm@gmail.com>
2024-08-19 07:57:08 +0200

    I want to move here code to simulate fake data.

---
+++ R/sim.R
@@ -0,0 +1,24 @@
+#' Generate random cross-sectional dataset
+#'
+#' @param N Number of subjects. An integer.
+#' @param Q Number of exposure variables. An integer.
+#'
+#' @return
+#' @export
+cross_sectional_data <- function(N, Q) {
+
+} # End cross_sectional_data
+################################################################################
+
+#' Generate random longitudinal dataset
+#'
+#' @param N Number of subjects. An integer.
+#' @param Q Number of exposure variables for each time point. An integer.
+#' @param P Number of time points. An integer.
+#'
+#' @return
+#' @export
+longitudinal_data <- function(N, Q, P) {
+
+} # End longitudinal_data
+################################################################################
